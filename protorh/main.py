4ad6648baddb6e4f90c12761b0d22e67553c01b2
mathis diallo-themista <mathisdiallo-themista@mbp-de-mathis.home>
2023-10-23 17:14:20 +0200

    1ère endpoint + problème d'execution de l'API

--- protorh/main.py
+++ protorh/main.py
@@ -1,2 +1,14 @@
 import subprocess, uvicorn
-from sqlalchemy import create_engine
+from fastapi import FastAPI
+from sqlalchemy import create_engine, Column, Integer, String, Float, text
+from sqlalchemy.orm import sessionmaker
+from sqlalchemy.ext.declarative import declarative_base
+from sqlalchemy_utils import database_exists, create_database
+from pydantic import BaseModel
+
+app = FastAPI()
+
+
+@app.get("/")
+async def read_root():
+    return {"Hello world !"}
