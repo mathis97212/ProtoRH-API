a87f1b9f74ceef57ac8bd7cdea395ffddb92b89e
mathis diallo-themista <mathisdiallo-themista@MacBook-Pro-de-mathis.local>
2023-10-24 09:21:44 +0200

    ajout des commentaires

--- protorh/main.py
+++ protorh/main.py
@@ -6,9 +6,13 @@ from sqlalchemy.ext.declarative import declarative_base
 from sqlalchemy_utils import database_exists, create_database
 from pydantic import BaseModel
 
+DATABASE_URL = "postgresql://jawa:123@localhost/ProtoRH"
+
 app = FastAPI()
 
-
+# Endpoint : /
+# Type : GET
+# this endpoint return à json string containing "Hello world !"
 @app.get("/")
 async def read_root():
     return {"Hello world !"}
