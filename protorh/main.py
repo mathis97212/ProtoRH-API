3f3fae5234096520b23bc45958c3f6c2a5b6b543
mathis diallo-themista <mathisdiallo-themista@MacBook-Pro-de-mathis.local>
2023-10-24 09:30:19 +0200

    update

--- protorh/main.py
+++ protorh/main.py
@@ -1,12 +1,16 @@
 import subprocess, uvicorn
 from fastapi import FastAPI
-from sqlalchemy import create_engine, Column, Integer, String, Float, text
+from sqlalchemy import create_engine, Column, Integer, String, Float, text, engine
 from sqlalchemy.orm import sessionmaker
 from sqlalchemy.ext.declarative import declarative_base
 from sqlalchemy_utils import database_exists, create_database
 from pydantic import BaseModel
 
 DATABASE_URL = "postgresql://jawa:123@localhost/ProtoRH"
+
+create = create_engine(DATABASE_URL)
+if not database_exists(engine.url):
+    create_database(engine.url, template="template0")
 
 app = FastAPI()
 
