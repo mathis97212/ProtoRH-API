405c34f986adf6d79e678279635220b7fc2079e0
saidi_a@etna-alternance.netemacs <saidi_a@etna-alternance.net>
2023-10-28 14:42:51 +0200

    user.py

--- main.py
+++ main.py
@@ -1,136 +1,175 @@
 import subprocess, uvicorn
+import os
 from fastapi import FastAPI
-from sqlalchemy import create_engine, Column, Integer, String, Float, text, engine
+from sqlalchemy import create_engine, Column, Integer, String, Date, Boolean, text, JSON, URL
 from sqlalchemy.orm import sessionmaker
 from sqlalchemy.ext.declarative import declarative_base
 from sqlalchemy_utils import database_exists, create_database
 from pydantic import BaseModel
 
-DATABASE_URL = "postgresql://jawa:123@localhost/ProtoRH"
+from Class.departement import Department, AddUserToDepartment, RemoveUserFromDepartment, GetUsersInDepartment
+from Class.user import User, Create, Update, UpdatePassword, UploadProfilePicture
+from Class.requestrh import RemoveRequestRH, UpdateRequestRH, RequestRH, GetRequestRH
+from Class.event import Event, CreateEvent, GetEvent, RemoveEvent
 
-create = create_engine(DATABASE_URL)
-if not database_exists(engine.url):
-    create_database(engine.url, template="template0")
+from routes import create_routes
+
+import routes
+
+import hashlib
+
+from dotenv import load_dotenv, dotenv_values
+
+Base = declarative_base
+
+salt = os.getenv("SALT")
+
+url_object = URL.create(
+    "postgresql",
+    username=os.getenv("DATABASE_USER"),
+    password=os.getenv("DATABASE_PASSWORD"),
+    host=os.getenv("DATABASE_HOST"),
+    database=os.getenv("DATABASE_NAME")
+)
+
+engine = create_engine(url_object)
+
+app = FastAPI()
+
+create_routes(app, engine)
 
 SessionLocal = sessionmaker(autocommit = False, autoflush=False, bind=engine)
-base = declarative_base
+Base = declarative_base()
 
-app = FastAPI()
-
-# Endpoint : /
-# Type : GET
-# this endpoint return à json string containing "Hello world !"
-@app.get("/")
+@app.get("/hello")
 async def read_root():
-    return {"Hello world !"}
+    return {"Hello, World !"}
 
-from fastapi import FastAPI, HTTPException, Request
-from pydantic import BaseModel
-import jwt
-import datetime
+@app.get("/exit")
+async def hello():
+    subprocess.call(["pkill", "uvicorn"])
+    return {"message": "Server Stopped"}
 
-app = FastAPI()
+@app.post("/user/create", response_model=tuple)
+async def Create(user: UserCreate):
+    if user.password != user.password_repeat:
+        return {
+            "message" : "Les mots de passe ne correspondent pas"
+        }
+    query = text("INSERT INTO users (Email, Password, Firstname, Lastname, BirthdayDate, Adress, PostalCode, Age, Meta, Token, Role) VALUES (:email, :password, :firstname, :lastname, :birthday_date, :adress, :postal_code, :age, :meta, :token, :role) RETURNING *")
+    salted = user.password + salt
+    values = {
+        "email" : user.email,
+        "password" : str(md5(salted.encode('utf-8')).digest()),
+        "firstname" : user.firstname,
+        "lastname" : user.lastname,
+        "birthday_date" : user.birthday_date,
+        "adress" : user.adress,
+        "postal_code" : user.postal_code,
+        "age" : datetime.now() - user.birthday_date,
+        "meta" : "{}",
+        "token" : hash_djb2(user.email + user.firstname + user.lastname + salt),
+        "role" : "user",
 
-# Clé secrète pour JWT (à remplacer par votre clé réelle)
-SECRET_KEY = 'votre_clé_secrète_jwt'
-
-# Base de données (simulation)
-demandes_rh = []
-
-# Modèles Pydantic pour les données
-class DemandeRH(BaseModel):
-    id_user: int
-    content: str
-
-# Middleware pour vérifier le JWT
-def verify_jwt(token):
-    try:
-        payload = jwt.decode(token, SECRET_KEY, algorithms=['HS256'])
-        return payload
-    except jwt.ExpiredSignatureError:
-        return 'Jetons JWT expiré'
-    except jwt.InvalidTokenError:
-        return 'Jetons JWT non valides'
-
-# Endpoint création  d'une demande RH
-@app.post('/rh/msg/add')
-def add_demande_rh(demande: DemandeRH, request: Request):
-    token = request.headers.get('Authorization')
-    payload = verify_jwt(token)
-    if not payload:
-        raise HTTPException(status_code=401, detail='Accès refusé')
-
-    current_date = datetime.datetime.now()
-
-    nouvelle_demande = {
-        'registration_date': current_date.isoformat(),
-        'visibility': True,
-        'close': False,
-        'last_action': current_date.isoformat(),
-        'content': [
-            {
-                'author': demande.id_user,
-                'content': demande.content,
-                'date': current_date.isoformat()
-            }
-        ]
     }
+    with engine.begin() as comm:
+        try :
+            result = comm.execute(query, values)
+        except exc.IntegrityError:
+            raise mail_exception
+        return result.fetchone()
+    
+@app.post("/connect")
+async def connect(infos : UserConnect):
+    query = text("SELECT Email, Firstname, Lastname, Role, Token FROM users WHERE Email = :email AND Password = :password;")
+    salted = infos.password + salt
+    values = {
+        "email" : infos.email,
+        "password" : str(md5(salted.encode('utf-8')).digest())
+    }
+    with engine.begin() as comm:
+        result = comm.execute(query, values)
+        user = result.fetchone()
+        if not user:
+            raise HTTPException(status_code=401, detail="Incorrect mail or password", headers={"WWW-Authenticate": "Bearer"})
+        JWT = jwt.encode({
+            "exp": datetime.now(timezone.utc) + timedelta(hours=1),
+            "Email": user.email,
+            "Firstname": user.firstname,
+            "Lastname": user.lastname,
+            "Role" : user.role,
+            "token": user.token
+            }, SECRET_KEY, algorithm="HS256")
+        return {"auth_token": JWT, "token_type": "bearer"}
 
-    demandes_rh.append(nouvelle_demande)
+@app.get("/user/{user_id}", response_model=getUser)
+async def GetUser(JWT: Annotated[str, Depends(oauth2_scheme)], user_id: int):
+    try:
+        authenticatedUser = Authorize(JWT)
+    except HTTPException:
+        raise credentials_exception
+    if authenticatedUser.role == 'admin' :
+        query = text("SELECT * FROM usersAdminView WHERE id = :user_id")
+    else:
+        query = text("SELECT * FROM usersView WHERE id = :user_id")
+    values = {
+        "user_id" : user_id
+    }
+    with engine.begin() as comm:
+        result = comm.execute(query, values)
+        user = result.fetchone()
+    if not user:
+        raise user_exception
+    return user
 
-    return nouvelle_demande
-# Endpoint demande RH non visible
-@app.post('/rh/msg/remove')
-def remove_demande_rh(message_id: int, request: Request):
-    token = request.headers.get('Authorization')
-    payload = verify_jwt(token)
-    if not payload:
-        raise HTTPException(status_code=401, detail='Accès refusé')
-
-    for demande_rh in demandes_rh:
-        if demande_rh['content'][0]['author'] == payload['user_id']:
-            # La demande est considérée  comme non visible
-            demande_rh['visibility'] = False
-            # Maj last_action et delete_date
-            current_date = datetime.datetime.now()
-            demande_rh['last_action'] = current_date.isoformat()
-            demande_rh['delete_date'] = current_date.isoformat()
-
-    return 'Demande marquée comme non visible'
-
-# Endpoint maj une demande RH
-@app.post('/rh/msg/update')
-def update_demande_rh(demande: DemandeRH, request: Request):
-    token = request.headers.get('Authorization')
-    payload = verify_jwt(token)
-    if not payload:
-        raise HTTPException(status_code=401, detail='Accès refusé')
-
-    current_date = datetime.datetime.now()
-
-    for demande_rh in demandes_rh:
-        if demande_rh['content'][0]['author'] == demande.id_user:
-            demande_rh['last_action'] = current_date.isoformat()
-            new_content = {
-                'author': demande.id_user,
-                'content': demande.content,
-                'date': current_date.isoformat()
-            }
-            demande_rh['content'].append(new_content)
-
-    return 'Demande mise à jour avec succès'
-
-# Endpoint pour récupérer les demandes RH
-@app.get('/rh/msg')
-def get_demandes_rh(request: Request):
-    token = request.headers.get('Authorization')
-    payload = verify_jwt(token)
-    if not payload:
-        raise HTTPException(status_code=401, detail='Accès refusé')
-
-    if payload['role'] not in ['manager', 'admin']:
-        raise HTTPException(status_code=403, detail='Accès refusé : seuls les managers et les administrateurs peuvent voir les demandes')
-
-    # jointure avec les utilisateurs si nécessaire
-    # renvoi simplement les demandes RH existantes
-    return demandes_rh
+@app.post("/user/update", response_model=updateUser)
+async def UpdateUser(JWT: Annotated[str, Depends(oauth2_scheme)], update : dict):
+    try:
+        authenticatedUser = Authorize(JWT)
+    except HTTPException:
+        raise credentials_exception
+    user_id = authenticatedUser.id
+    select = text("SELECT * FROM usersAdminView WHERE id = :user_id")
+    if update.get("id"):
+        try:
+            uid = int(update.get("id"))
+        except ValueError:
+            raise id_exception
+        if authenticatedUser.role != 'admin' and update.get("id") != authenticatedUser.id:
+            HTTPException(
+                status_code=403,
+                detail=f"Unauthorized to update this user",
+                headers={"WWW-Authenticate": "Bearer"},
+            )
+        user_id = update.get("id")
+    select_values = {
+        "user_id" : user_id
+    }
+    with engine.begin() as comm:
+        try :
+           select_result = comm.execute(select, select_values).mappings().one()
+        except exc.NoResultFound:
+            raise user_exception
+    update.update(select_result, **update)
+    if authenticatedUser.role == "admin":
+        query = text("UPDATE users SET Email = :email, Firstname = :firstname, Lastname = :lastname, BirthdayDate = :birthdaydate, Age = :age, Adress = :adress, Role = :role WHERE id = :user_id RETURNING *")
+    else:
+        query = text("UPDATE users SET Email = :email, BirthdayDate = :birthdaydate, Adress = :adress WHERE id = :user_id RETURNING *")
+    age : date = date.today().year - update.get("birthdaydate").year
+    values = {
+        "email" : update.get("email"),
+        "firstname" : update.get("firstname"),
+        "lastname" : update.get("lastname"),
+        "birthdaydate" : update.get("birthdaydate"),
+        "adress" : update.get("address"),
+        "age" : age,
+        "role" : update.get("role"),
+        "user_id" : user_id
+    }
+    with engine.begin() as comm:
+        try :
+            result = comm.execute(query, values)
+        except exc.IntegrityError:
+            raise mail_exception
+        response = result.fetchone()
+        return response
