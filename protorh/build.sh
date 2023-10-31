91b9fe9f231caa0280a050a74d89a7cdd55a55b3
saidi_a@etna-alternance.netemacs <saidi_a@etna-alternance.net>
2023-10-31 09:14:52 +0100

    user.py

--- protorh/build.sh
+++ protorh/build.sh
@@ -1,7 +1,6 @@
 pip3 install -r requirements.txt --break-system-packages
-apt-get services start postgresql
+apt services start postgresql
 createdb protorh -U app
 psql -d protorh -f database_rh.psql
 #psql -U app --password -d protorh -f database_rh.psql
-apt-get services stop postgresql
-
+apt services stop postgresql
