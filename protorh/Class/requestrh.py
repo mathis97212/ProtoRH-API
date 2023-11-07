9aa53d10c41b35d0764ef32c7fafca621a6ab8cf
saidi_a@etna-alternance.netemacs <saidi_a@etna-alternance.net>
2023-11-07 18:25:48 +0100

    maj

--- protorh/Class/requestrh.py
+++ protorh/Class/requestrh.py
@@ -1,64 +1,58 @@
-# Importation des modules et bibliothèques nécessaires.
-import subprocess  # Pour lancer des sous-processus (non utilisé dans ce code).
-import uvicorn  # Pour exécuter l'application FastAPI avec Uvicorn.
-import os  # Pour interagir avec le système d'exploitation.
-from sqlalchemy import create_engine, Column, Integer, Float, String, text, Date, JSON, Boolean  # Pour la gestion de la base de données.
-from sqlalchemy.orm import sessionmaker  # Pour créer des sessions de base de données.
-from sqlalchemy.ext.declarative import declarative_base  # Pour créer une classe de base pour les classes de modèle.
-from sqlalchemy_utils import database_exists, create_database  # Pour la gestion de la base de données.
-from pydantic import BaseModel  # Pour définir des modèles Pydantic.
-from fastapi import FastAPI  # Importation de la classe FastAPI.
-from datetime import date  # Pour travailler avec les dates.
+import subprocess, uvicorn
+import os
+from sqlalchemy import create_engine, Column, Integer, Float, String, text, Date, JSON, Boolean
+from sqlalchemy.orm import sessionmaker
+from sqlalchemy.ext.declarative import declarative_base
+from sqlalchemy_utils import database_exists, create_database
+from pydantic import BaseModel
+from fastapi import FastAPI
+from datetime import date
 
-# Création d'une classe de modèle de base SQLAlchemy.
 Base = declarative_base()
 
-# Définition de la classe "RequestRH" qui correspond à la table "RequestRH" dans la base de données.
 class RequestRH(Base):
     __tablename__ = "RequestRH"
-    id = Column(Integer, primary_key=True, index=True)
-    UserID = Column(Integer)
-    Content = Column(String)
-    RegistrationDate = Column(Date)
-    Visibility = Column(Boolean)
-    Close = Column(Boolean)
-    LastAction = Column(Boolean)
-    ContentHistory = Column(JSON)
-
-# Définition des classes Pydantic pour gérer les opérations liées aux demandes RH.
+    id = Column(Integer, primary_key = True, index=True)
+    user_id = Column(Integer)
+    content = Column(String)
+    registrationdate = Column(Date)
+    visibility = Column(Boolean)
+    close = Column(Boolean)
+    lastaction = Column(Boolean)
+    contenthistory = Column(JSON)
+    
 class CreateRequestRH(BaseModel):
-    UserID: int
-    Content: str
-    RegistrationDate: date
-    Visibility: bool
-    Close: bool
-    LastAction: bool
-    ContentHistory: str
-
+    user_id : int
+    content : str
+    lastaction : date
+    contenthistory : str
+    
+    
 class UpdateRequestRH(BaseModel):
-    id: int
-    UserID: int
-    Content: str
-    RegistrationDate: date
-    Visibility: str
-    Close: str
-    LastAction: str
-    ContentHistory: str
+    id : int 
+    user_id : int
+    Content : str
+    registrationdate : date
+    visibility : str
+    close : str
+    lastaction : str
+    contenthistory : str
 
 class RemoveRequestRH(BaseModel):
-    UserID: int
-    Content: str
-    RegistrationDate: date
-    Visibility: str
-    Close: str
-    LastAction: str
-    ContentHistory: str
+    user_id : int
+    content : str
+    registrationdate : date
+    visibility : str
+    close : str
+    lastaction : str
+    contenthistory : str
+
 
 class GetRequestRH(BaseModel):
-    UserID: int
-    Content: str
-    RegistrationDate: date
-    Visibility: str
-    Close: str
-    LastAction: str
-    ContentHistory: str
+    user_id : int
+    content : str
+    registrationdate : date
+    visibility : str
+    close : str
+    lastaction : str
+    contenthistory : str
